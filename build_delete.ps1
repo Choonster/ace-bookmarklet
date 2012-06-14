27783aa12850376d7dea6dbcef2d71c666d740da
Choonster <Choonster.2010@gmail.com>
2012-06-14 21:00:11 +1000

    Renamed LuaHTML to LuaPage, fixed newline issues

--- build_delete.ps1
+++ build_delete.ps1
@@ -1,3 +1,16 @@
-﻿Remove-Item -Recurse .\build\*
+﻿# Remove the old built code
+Remove-Item -Recurse .\build\*
+
+# Build Ace again
 npm install
 node Makefile.dryice.js full
+
+# Fix the newline escaping issues in the built code (replace each newline followed by \n with \n)
+foreach ($path in @("src\ace.js", "src-noconflict\ace.js", "textarea\src\ace-bookmarklet.js"))
+{
+	$fullpath = ".\build\" + $path
+	"Fixing $fullpath"
+	$ace = [string]::Join("`n", (get-content $fullpath))
+	$ace = $ace -replace "`n\\n", "\n"
+	$ace > $fullpath
+}
---
+++ build_delete.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/env bash
+
+# Remove the old build code
+rm ./build/*
+
+# Build Ace again
+npm install
+node Makefile.dryice.js full
+
+# Fix the newline escaping issues in the built code (replace each newline followed by \n with \n)
+paths = ( "src/ace.js" "src-noconflict/ace.js" "textarea/src/ace-bookmarklet.js" )
+for path in "$paths[@]"
+do
+	fullpath = "./build/${path}"
+	echo "Fixing ${fullpath}"
+	sed -e :a -e '$!N;s/\n\\n/\\n/;ta' "${fullpath}"
+	# Copy-pasted from the blog below. No idea if it will work.
+	# http://unstableme.blogspot.com.au/2008/05/remove-or-replace-newlines-using.html
+done
